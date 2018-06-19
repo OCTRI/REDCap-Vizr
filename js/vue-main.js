ae82995195df7abc96c5fc051507f5d605d7b4cb
Heath Harrelson <harrelst@ohsu.edu>
2018-06-19 10:58:47 -0700

    Implement when no charts and user can edit
    Most tests disabled; will fill in incrementally.

--- js/vue-main.js
+++ js/vue-main.js
@@ -3,6 +3,11 @@ import Vizr from './components/Vizr';
 
 new Vue({
   el: '.vizr-container',
+  data: {
+    // TODO: get these from php
+    pid: 13,
+    canEdit: true
+  },
   components: { Vizr },
-  template: '<Vizr/>'
+  template: '<Vizr :pid="pid" :can-edit="canEdit"/>'
 });
