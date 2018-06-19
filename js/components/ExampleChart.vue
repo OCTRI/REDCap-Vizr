6e8854d12d9fbeebf2abe8c46148a217cc335192
Heath Harrelson <harrelst@ohsu.edu>
2018-06-19 10:40:05 -0700

    ExampleChart Component
    Implement a component that displays the example chart.

---
+++ js/components/ExampleChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <figure class="pull-right vizr-example">
+    <img src="lib/vizr_example.png" :alt="messages.imgTitle" class="img-thumbnail" :title="messages.imgTitle">
+    <figcaption>
+      <strong>{{ messages.captionIntro }}</strong> {{ messages.captionTitle }}
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  name: 'ExampleChart',
+
+  data() {
+    return {
+      messages: {
+        captionIntro: 'Example',
+        captionTitle:  ': This charted data has a screen date, is true for screened in = yes, and has a Trend Line comparing collected data to a target count',
+        imgTitle: 'Example Vizr Chart - Screened In Results'
+      }
+    };
+  }
+}
+</script>
