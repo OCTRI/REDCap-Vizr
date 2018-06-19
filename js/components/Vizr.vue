973872e4d7b6fb9694b919c2b53b16f63d5389e2
Heath Harrelson <harrelst@ohsu.edu>
2018-06-19 10:58:47 -0700

    Implement when no charts and user can edit
    Most tests disabled; will fill in incrementally.

--- js/components/Vizr.vue
+++ js/components/Vizr.vue
@@ -2,10 +2,82 @@
   <div>
     <h1>Vizr Charts</h1>
 
-    <div id="vizr-instructions">TODO instructions</div>
+    <ExampleChart v-show="noCharts"/>
+    <Instructions :can-edit="canEdit" :has-charts="hasCharts"/>
 
     <div class="vizr-charts">
-      TODO charts
     </div>
+
+    <div class="spacious">
+      <button type="button"
+              class="btn btn-lg btn-warning"
+              data-toggle="collapse"
+              data-target=".add-chart-form"
+              aria-expanded="false"
+              aria-controls="add-chart-form"
+      >
+        {{ messages.actions.create }}
+      </button>
+    </div>
+
+    <!-- TODO new chart form -->
+
+    <VizrVersion/>
   </div>
 </template>
+
+<script>
+import ExampleChart from './ExampleChart';
+import Instructions from './Instructions';
+import VizrVersion from './VizrVersion';
+
+const messages = {
+  actions : {
+    confirmDelete: 'Permanently delete chart',
+    create: 'Build a Chart',
+    delete: 'Delete',
+    edit: ' Edit Chart',
+    toggleLegend: 'Hide/Show Legend'
+  },
+  warnings: {
+    blankDateFields: ((count) => `Ignored ${count} records with blank date field.`),
+    multipleEvents: 'The filter returned multiple events per record.',
+    noData: 'The filter returned 0 records.',
+    repeatingInstruments: 'Charts may not work as expected with repeating instruments.',
+    responseError: 'Unable to generate a chart.'
+  }
+};
+
+export default {
+  name: 'Vizr',
+
+  props: {
+    pid: Number,
+    canEdit: Boolean
+  },
+
+  data() {
+    return {
+      messages,
+      config: {},
+    };
+  },
+
+  computed: {
+    hasCharts() {
+      const config = this.config;
+      return !!(config.charts && config.charts.length);
+    },
+
+    noCharts() {
+      return !this.hasCharts;
+    }
+  },
+
+  components: {
+    ExampleChart,
+    Instructions,
+    VizrVersion
+  }
+}
+</script>
