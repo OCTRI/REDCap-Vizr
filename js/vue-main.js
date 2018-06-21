fa770d86715e7525eccc87f189f21c45a0f6f3b3
Heath Harrelson <harrelst@ohsu.edu>
2018-06-21 11:14:39 -0700

    Parameter Injection
    Inject pid, `canEdit` flag, and asset URLs as in the vanilla JS
    module.

--- js/vue-main.js
+++ js/vue-main.js
@@ -1,13 +1,32 @@
 import Vue from 'vue';
+
+import '../lib/vizr.css';
 import Vizr from './components/Vizr';
 
-new Vue({
-  el: '.vizr-container',
-  data: {
-    // TODO: get these from php
-    pid: 13,
-    canEdit: true
-  },
-  components: { Vizr },
-  template: '<Vizr :pid="pid" :can-edit="canEdit"/>'
-});
+/**
+ * Main method that will create all the charts and populate them with data.
+ *
+ * @param {Number} pid - project id; used to construct the data queries.
+ * @param {Boolean} canEdit - boolean indicating whether or not the links should display to
+ *   create or edit charts.
+ * @param {String} jsonAssetUrls - JSON-encoded string containing key/value pairs where
+ *   the key is a relative file path like `lib/data.php`, and the value is the corresponding
+ *   external module URL like `http://localhost/redcap/api/?type=module&prefix=vizr&page=lib%2Fdata&pid=14`
+ */
+export function run(pid, canEdit, jsonAssetUrls) {
+  const parsedAssetUrls = JSON.parse(jsonAssetUrls);
+
+  /**
+   * Instantiate the Vue component tree.
+   */
+  new Vue({
+    el: '.vizr-container',
+    data: {
+      pid,
+      canEdit,
+      jsonAssetUrls: parsedAssetUrls
+    },
+    components: { Vizr },
+    template: '<Vizr :pid="pid" :can-edit="canEdit"/>'
+  });
+}
