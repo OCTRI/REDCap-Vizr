21c6708a1e8a8adeb91b9cb108f6e14086907bc7
Heath Harrelson <harrelst@ohsu.edu>
2018-06-22 14:47:56 -0700

    Data Service
    Initial implementation of a service to encapsulate requests to
    REDCap.

---
+++ js/services/data-service.js
@@ -0,0 +1,99 @@
+import assert from 'assert';
+import axios from 'axios';
+import qs from 'qs';
+
+export const ENDPOINTS = {
+  CHART_DATA: 'lib/data.php',
+  CONFIG: 'lib/chart_defs.php',
+  METADATA: 'lib/metadata.php',
+  PERSISTENCE: 'lib/persist.php'
+};
+
+/**
+ * Constructs a service that fetches data for the current project from REDCap.
+ *
+ * @param {Object} assetUrls - key/value pairs where the key is a relative file path
+ *   like `lib/data.php`, and the value is the corresponding external module URL
+ *   like `http://localhost/redcap/api/?type=module&prefix=vizr&page=lib%2Fdata&pid=14`
+ * @return {Object} an object encapsulating REDCap HTTP requests
+ */
+export default function createDataService(assetUrls) {
+  assert(assetUrls, 'Asset URL object is required');
+  Object.keys(ENDPOINTS).forEach(key => {
+    const endpoint = ENDPOINTS[key];
+    assert(assetUrls[endpoint], `A URL for ${endpoint} is required`)
+  });
+
+  return {
+    metadataUrl: assetUrls[ENDPOINTS.METADATA],
+    configUrl: assetUrls[ENDPOINTS.CONFIG],
+    persistenceUrl: assetUrls[ENDPOINTS.PERSISTENCE],
+    dataUrl: assetUrls[ENDPOINTS.CHART_DATA],
+
+    /**
+     * Logs the response for debugging.
+     * @param {Promise->Object} response - an axios response object.
+     * @return {Promise->Object} the unchanged input response
+     * @private
+     */
+    _logResponse(response) {
+      // eslint-disable-next-line no-console
+      console.log(response);
+      return response;
+    },
+
+    /**
+     * Extracts data returned by the request.
+     * @param {Promise->Object} response - an axios response object.
+     * @return {Promise->Object} the response's data
+     * @private
+     */
+    _extractData(response) {
+      return response.data;
+    },
+
+    /**
+     * Gets the metadata for the current project.
+     *
+     * @return {Promise-> {recordIdField: string, dataDictionary: Object, charts: Object[]}}
+     *   Returns a promise that resolves to a configuraton object containing the project's
+     *   recordIdField, dataDictionary, and events.
+     */
+    getMetadata() {
+      return axios(this.metadataUrl)
+        .then(this._extractData);
+    },
+
+    /**
+     * Gets the current project's chart definitions from the external module settings.
+     *
+     * @return {Promise -> {error: String} | {charts: Object[]}} -
+     *   Returns a promise that configuration object with either the single key 'error' or
+     *   an array of chart configuration objects.
+     */
+    getChartConfig() {
+      return axios(this.configUrl)
+        .then(this._extractData)
+        .then(data => {
+          const { configArray } = data;
+          const emptyConfig = Boolean(configArray && configArray.length < 1);
+          let charts = emptyConfig ? [] : configArray;
+          return { charts };
+        });
+    },
+
+    /**
+     * Gets data from the REDCap project for constructing a chart.
+     *
+     * @param {{recordIdField: string, filter: string, events: string[], fields: string[] }} -
+     *   an object with metadata and chart configuraton needed to perform the data request.
+     * @return {Promise-> {data: Object, filterEvents: string[]}} A promise that resolves to
+     *   an object with a key for 'data' and 'filterEvents' if the  project is longitudinal.
+     */
+    getChartData(requestBody) {
+      // TODO validate / filter response data (main.js line 460)
+      return axios.post(this.dataUrl, qs.stringify(requestBody))
+        .then(this._extractData);
+    }
+  };
+}
---
+++ test/services/data-service_test.js
@@ -0,0 +1,95 @@
+import 'jasmine-ajax';
+import { exampleResponses } from '../example-ajax-responses';
+import createDataService, { ENDPOINTS } from '@/services/data-service';
+
+describe('data service', () => {
+  let mockUrls;
+
+  beforeEach(() => {
+    mockUrls = {};
+    Object.keys(ENDPOINTS).forEach(key => {
+      mockUrls[ENDPOINTS[key]] = '/' + ENDPOINTS[key];
+    });
+  });
+
+  it('createDataService requires asset URLs', () => {
+    expect(() => createDataService()).toThrowError(/Asset URL object is required/);
+  });
+
+  it('createDataService requires every endpoint to have a URL', () => {
+    mockUrls[ENDPOINTS.PERSISTENCE] = null;
+    expect(() => createDataService(mockUrls))
+      .toThrowError(/A URL for lib\/persist.php is required/);
+  });
+
+  describe('requests', () => {
+    let service;
+
+    beforeEach(function() {
+      service = createDataService(mockUrls);
+      jasmine.Ajax.install();
+    });
+
+    afterEach(function() {
+      jasmine.Ajax.uninstall();
+    });
+
+    it('getMetadata', (done) => {
+      const expectedUrl = mockUrls[ENDPOINTS.METADATA];
+      jasmine.Ajax.stubRequest(expectedUrl)
+        .andReturn(exampleResponses.metadata.longitudinal);
+
+      service.getMetadata().then(metadata => {
+        const request = jasmine.Ajax.requests.mostRecent();
+        expect(request.url).toEqual(expectedUrl);
+
+        // it resolves with the metadata object
+        expect(metadata.recordIdField).toBeDefined();
+        expect(metadata.dataDictionary).toBeDefined();
+        expect(metadata.events).toBeDefined();
+
+        done();
+      });
+    });
+
+    it('getChartConfig', (done) => {
+      const expectedUrl = mockUrls[ENDPOINTS.CONFIG];
+      jasmine.Ajax.stubRequest(expectedUrl)
+        .andReturn(exampleResponses.config.longitudinal);
+
+      service.getChartConfig().then(chartConfig => {
+        const request = jasmine.Ajax.requests.mostRecent();
+        expect(request.url).toEqual(expectedUrl);
+
+        // it resolves with the chart configuration
+        expect(chartConfig.error).not.toBeDefined();
+        expect(chartConfig.charts).toBeDefined();
+        expect(chartConfig.charts.length).toEqual(1);
+
+        done();
+      });
+    });
+
+    it('getChartData', (done) => {
+      const expectedUrl = mockUrls[ENDPOINTS.CHART_DATA];
+      const expectedContentType = 'application/x-www-form-urlencoded';
+      jasmine.Ajax.stubRequest(expectedUrl)
+        .andReturn(exampleResponses.data.longitudinal);
+
+      service.getChartData({ a: 'b' }).then(chartData => {
+        const request = jasmine.Ajax.requests.mostRecent();
+        expect(request.url).toEqual(expectedUrl);
+
+        // request parameters are URL encoded
+        expect(request.requestHeaders['Content-Type']).toEqual(expectedContentType);
+        expect(request.params).toEqual('a=b');
+
+        // it extracts data from the response
+        expect(chartData.filterEvents).toBeDefined();
+        expect(chartData.data).toBeDefined();
+
+        done();
+      });
+    });
+  });
+});
