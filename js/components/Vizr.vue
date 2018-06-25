a13664efee5d93e0bd7ff4d955f5a508331b0417
Heath Harrelson <harrelst@ohsu.edu>
2018-06-25 12:08:46 -0700

    Load Config on Mount
    Load the project's metadata and chart configuration when the Vizr
    component is mounted.

--- js/components/Vizr.vue
+++ js/components/Vizr.vue
@@ -1,71 +1,104 @@
 <template>
-  <div id="vizr-container">
-    <h1>Vizr Charts</h1>
+  <transition name="fade">
+    <div id="vizr-container" v-if="!loading">
+      <h1>Vizr Charts</h1>
 
-    <ExampleChart v-show="noCharts"/>
-    <Instructions :can-edit="canEdit" :has-charts="hasCharts"/>
+      <ExampleChart v-show="noCharts"/>
+      <Instructions :can-edit="canEdit" :has-charts="hasCharts"/>
 
-    <div class="error" v-if="hasConfigError">
-      {{ config.error }}
+      <div class="error" v-if="hasConfigError">
+        {{ messages.warnings.configError }} {{ config.error.message }}
+      </div>
+
+      <div class="vizr-charts">
+        <Chart v-for="chart in charts" :key="chart.id" :metadata="metadata" :config="chart"/>
+      </div>
+
+      <div class="spacious" v-if="canEdit">
+        <button type="button"
+                class="btn btn-lg btn-warning"
+                data-toggle="collapse"
+                data-target=".add-chart-form"
+                aria-expanded="false"
+                aria-controls="add-chart-form"
+                :disabled="hasConfigError"
+        >
+          {{ messages.actions.create }}
+        </button>
+      </div>
+
+      <!-- TODO new chart form -->
+
+      <VizrVersion/>
     </div>
-
-    <div class="vizr-charts">
-    </div>
-
-    <div class="spacious">
-      <button type="button"
-              class="btn btn-lg btn-warning"
-              data-toggle="collapse"
-              data-target=".add-chart-form"
-              aria-expanded="false"
-              aria-controls="add-chart-form"
-      >
-        {{ messages.actions.create }}
-      </button>
-    </div>
-
-    <!-- TODO new chart form -->
-
-    <VizrVersion/>
-  </div>
+  </transition>
 </template>
 
 <script>
+import Chart from './Chart';
 import ExampleChart from './ExampleChart';
 import Instructions from './Instructions';
 import VizrVersion from './VizrVersion';
 
 const messages = {
   actions : {
-    confirmDelete: 'Permanently delete chart',
     create: 'Build a Chart',
-    delete: 'Delete',
-    edit: ' Edit Chart',
-    toggleLegend: 'Hide/Show Legend'
   },
   warnings: {
-    blankDateFields: ((count) => `Ignored ${count} records with blank date field.`),
-    multipleEvents: 'The filter returned multiple events per record.',
-    noData: 'The filter returned 0 records.',
-    repeatingInstruments: 'Charts may not work as expected with repeating instruments.',
-    responseError: 'Unable to generate a chart.'
+    configError: 'Project configuration could not be loaded due to an error: '
   }
 };
 
+/**
+ * Vizr root component. Fetches configuration for the project and renders the charts
+ * and forms.
+ */
 export default {
   name: 'Vizr',
+  inject: ['dataService'],
 
   props: {
     pid: Number,
     canEdit: Boolean
   },
 
+  components: {
+    Chart,
+    ExampleChart,
+    Instructions,
+    VizrVersion
+  },
+
   data() {
     return {
       messages,
+      loading: true,
       metadata: {},
       config: {}
     };
+  },
+
+  mounted() {
+    // capture the promise to synchronize tests
+    this.configPromise = this.fetchConfig();
+  },
+
+  methods: {
+    fetchConfig() {
+      const { dataService } = this;
+      return dataService.getProjectConfig()
+        .then(responseArray => {
+          const [ metadata, chartConfig ] = responseArray;
+          this.metadata = metadata;
+          this.config = chartConfig;
+        })
+        .catch(reason => {
+          this.config = { error: reason };
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    }
   },
 
   computed: {
@@ -81,13 +114,12 @@ export default {
 
     noCharts() {
       return !this.hasCharts;
-    }
-  },
+    },
 
-  components: {
-    ExampleChart,
-    Instructions,
-    VizrVersion
+    charts() {
+      const { config } = this;
+      return config && config.charts ? config.charts : [];
+    }
   }
 }
 </script>
