e078f2e1b316b93bd7c682613d676d4148e543a6
Heath Harrelson <harrelst@ohsu.edu>
2018-06-25 12:06:09 -0700

    Stub Chart Component
    Add a stub for the chart component.

---
+++ js/components/Chart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ config.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chart',
+
+  props: {
+    canEdit: Boolean,
+    config: Object,
+    metadata: Object
+  }
+};
+</script>
