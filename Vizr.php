37cda9da98b0c140f7d2598fdd6db53196469cbd
Weldon Sams <sams@ohsu.edu>
2018-06-29 17:08:36 -0700

    REDDEV-539 checks for module user rights
    Refactored Vizr.php and lib/permissions.php to also check for user rights.

--- Vizr.php
+++ Vizr.php
@@ -6,4 +6,43 @@ use ExternalModules\ExternalModules;
 
 class Vizr extends AbstractExternalModule {
 
+  /**
+   * Override to check for Vizr User Rights. Else returns the parent value
+   * which checks for design rights.
+   */
+  public function redcap_module_link_check_display($project_id, $link) {
+    if ($this->hasVizrUserRights()) {
+      return $link;
+    } else {
+      return parent::redcap_module_link_check_display($project_id, $link);
+    }
+  }
+
+  /**
+   * Checks to see if a user has user rights for the Vizr external module on
+   * the current project.
+   */
+  public function hasVizrUserRights() {
+    $rights = \REDCap::getUserRights(USERID);
+    $user_module_config = $rights[USERID]['external_module_config'];
+    return !empty($rights) && is_array($user_module_config)
+        && in_array($this->PREFIX, $user_module_config);
+  }
+
+  /**
+   * Checks to see if a user has design rights for the current project.
+   */
+  public function hasDesignRights() {
+    $rights = \REDCap::getUserRights(USERID);
+    return !empty($rights) && $rights[USERID]['design'];
+  }
+
+  /**
+   * Checks to see if the user can edit charts. A user can edit charts if they
+   * are a super user or have design or user rights for the vizr module.
+   */
+  public function canEditVizrCharts() {
+    return SUPER_USER || $this->hasDesignRights() || $this->hasVizrUserRights();
+  }
+
 }
--- lib/permissions.php
+++ lib/permissions.php
@@ -7,7 +7,4 @@
 // NOTE: The following line should be included in the calling page.
 // require_once "../../../redcap_connect.php";
 
-$rights = REDCap::getUserRights(USERID);
-$user_module_config = $rights[USERID]['external_module_config'];
-$has_module_rights = !empty($rights) && is_array($user_module_config) && in_array('vizr', $user_module_config);
-$can_edit = (SUPER_USER || (!empty($rights) && ($rights[USERID]['design'] || $has_module_rights)));
+$can_edit = $module->canEditVizrCharts();
