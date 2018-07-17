1db8be9abb093d84fc9632a14efa3f8f3188afed
Weldon Sams <wjsams@gmail.com>
2018-07-17 14:14:21 -0700

    REDDEV-539 refactor and documentation
    Refactored Vizr.php to allow view-only users.
    
    Updated documentation to include configuring user permissions.

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ in a time series fashion to provide additional insights about their project.
 # Vizr Installation and Upgrades
 
 See [INSTALL.md](INSTALL.md).
+
+# Vizr Permissions
+
+After installing Vizr you will need to configure the module to allow users to view, create and edit charts.
+
+First, navigate to *Control Center*, then *External Modules*, and click *Configure* for the Vizr module.
+
+Under *Module configuration permissions in projects* you have two options: *Require Project Setup/Design privilege* and *Require module-specific user privilege*. The first allows all users with *Project Design and Setup* rights full access to Vizr. They will be able to view, create, and edit charts. The second option should be used if you want some users to have view-only rights.
+
+For *Require Project Setup/Design privilege*, navigate to *User Rights*, select a user, and click *Edit user privileges*. Under *Highest level privileges* you will see *Project Design and Setup*. If this is checked the user will be able to view, create and edit charts. Notice, in this case checking *Project Design and Setup* also checks the Vizr module under the section *External Modules: Configuration Permissions* automatically - it's otherwise disabled.
+
+For *Require module-specific user privilege*, if you want a view-only user navigate to *User Rights*, select a user, and click *Edit user privileges*. Now select the Vizr module under *External Modules: Configuration Permissions* and save. If you also want that user to be able to create and edit charts you will also need to check *Project Design and Setup*. Both must be checked.
+
+If Vizr is enabled for a project, super users may view, create and edit charts in a all cases.
--- Vizr.php
+++ Vizr.php
@@ -7,6 +7,33 @@ use ExternalModules\ExternalModules;
 class Vizr extends AbstractExternalModule {
 
   /**
+   * Override to check for Vizr module rights in addition to design and
+   * super user rights checked by the parent. If a user has access to the
+   * Vizr module in User Rights they will be able to view charts. If the
+   * user has super user or design rights they will also be able to create
+   * and edit charts.
+   */
+  public function redcap_module_link_check_display($project_id, $link) {
+    $rights = \REDCap::getUserRights(USERID);
+    if ($this->hasVizrUserRights($rights)) {
+      return $link;
+    } else {
+      return parent::redcap_module_link_check_display($project_id, $link);
+    }
+  }
+
+  /**
+   * Checks to see if a user has module rights for Vizr on the current project.
+   * @param array $rights Array of user rights from <code>REDCap::getUserRights</code>.
+   */
+  public function hasVizrUserRights($rights) {
+    $user_module_config = $rights[USERID]['external_module_config'];
+    return !empty($rights) && is_array($user_module_config)
+        && in_array($this->PREFIX, $user_module_config);
+  }
+
+
+  /**
    * Checks to see if a user has design rights for the current project.
    * @param array $rights Array of user rights from <code>REDCap::getUserRights</code>.
    */
@@ -15,8 +42,8 @@ class Vizr extends AbstractExternalModule {
   }
 
   /**
-   * Checks to see if the user can edit charts. A user can edit charts if they
-   * are a super user or have design or user rights for the vizr module.
+   * Checks to see if the user can edit charts in Vizr. A user can edit charts if they
+   * are a super user or have design rights.
    */
   public function canEditVizrCharts() {
     $rights = \REDCap::getUserRights(USERID);
