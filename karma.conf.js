812634aa2ea0a77f0d4be72f77f89e420d4903ff
Heath Harrelson <harrelst@ohsu.edu>
2018-11-21 11:28:57 -0800

    Update Babel
    Update transpiler tooling to Babel 7.

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,8 @@
 
 const webpackConfig = require('./webpack.config.js');
 
+webpackConfig.mode = 'development';
+
 // Work around for a TypeError where Webpack passes an array including the bundle and the
 // source map to Karma:
 // https://github.com/webpack-contrib/karma-webpack/issues/322#issuecomment-417862717
@@ -12,28 +14,22 @@ webpackConfig.output = {
 
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
       'node_modules/jquery/dist/jquery.js',
-      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/@babel/polyfill/dist/polyfill.js',
       'test/**/*.js'
     ],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -41,29 +37,23 @@ module.exports = function(config) {
       'test/**/*.js': ['webpack']
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['spec'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
